a12c0520fa04cb350e1f836e0d1f44e9718a4e1b
liuminhui <huidbk@163.com>
2024-04-26 22:22:31 +0800

    代码优化

--- py_tools/utils/project_templates/python_project/src/settings/__init__.py
+++ py_tools/utils/project_templates/python_project/src/settings/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# @Author: Hui
-# @Desc: { 项目配置包初始化 }
-# @Date: 2023/09/07 16:38
 from .base_setting import server_host, server_log_level, server_port
 from .db_setting import (mysql_dbname, mysql_host, mysql_password, mysql_port, mysql_user, redis_db, redis_host,
                          redis_password, redis_port)
